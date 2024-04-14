eecba84432ac1e0f91b5cfc5e261a102fda835ee
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-14 17:28:31 +0200

    Edited Constructor body.

--- Classes-Objects/1stProg.cpp
+++ Classes-Objects/1stProg.cpp
@@ -7,10 +7,19 @@ class employee(){
     int salary;
 public:
     employee(string name, int age, int salary):name(name),age(age),salary(salary){}
-    
 
+    string getName(){
+        return name;
+    }
 
+    int age(){
 
+        return age;
+    }
+
+    int salary(){
+        return salary;
+    }
 
 }:
 
