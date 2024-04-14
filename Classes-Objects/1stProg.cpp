dce037dffe5449d34d4fe46acdd1e98128e0256a
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-14 17:26:13 +0200

    Added Class and Constructor.

--- Classes-Objects/1stProg.cpp
+++ Classes-Objects/1stProg.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
+class employee(){
+    string name;
+    int age;
+    int salary;
+public:
+    employee(string name, int age, int salary):name(name),age(age),salary(salary){}
+    
+
+
+
+
+}:
+
+
+
 
 int main(){
 
