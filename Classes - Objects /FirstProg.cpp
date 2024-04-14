4bc02ced9c7f1286158d7f50dba06ab9acd98846
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-14 14:12:03 +0200

    modified

--- Classes - Objects /FirstProg.cpp
+++ Classes - Objects /FirstProg.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 using namespace std;
 
+class FirstProg
+{
+private:
+    /* data */
+public:
+    FirstProg(/* args */);
+    ~FirstProg();
+};
+
+FirstProg::FirstProg(/* args */)
+{
+}
+
+FirstProg::~FirstProg()
+{
+}
+ Employee{
+
+
+
+};
+
+
 int main(){
-
-
-
-cout <<"Hello"<<endl;
 
 }
 
