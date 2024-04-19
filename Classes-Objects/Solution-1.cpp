a80b5561078d295d315a9e642165e20e92b79200
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-19 12:46:21 +0200

    Added Solution -1

---
+++ Classes-Objects/Solution-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+    double coord_x, coord_y;
+
+public:
+    double coord(double coord_x, double coord_y) : coord_x(coord_x), coord_y(coord_y) {}
+};
+
+int main()
+{
+}
