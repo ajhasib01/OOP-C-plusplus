b7ca6e8769d9df735333549a63cbe770e70cf55d
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-19 12:17:23 +0200

    Added branch solutions

--- Classes-Objects/1stProg.cpp
+++ Classes-Objects/1stProg.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
 using namespace std;
 
-class employee{
+class employee
+{
     string name;
     int age;
     int salary;
-public:
-    employee(const string &name, int age, int salary):name(name),age(age),salary(salary){}
 
-   const string &getName(){
+public:
+    employee(const string &name, int age, int salary) : name(name), age(age), salary(salary) {}
+
+    const string &getName()
+    {
         return name;
     }
 
-    int getAge(){
+    int getAge()
+    {
 
         return age;
     }
 
-    int getSalary(){
+    int getSalary()
+    {
         return salary;
     }
 
-    
-    void setName(const string &Newname){
-        if(Newname.length()<=10){
+    void setName(const string &Newname)
+    {
+        if (Newname.length() <= 10)
+        {
             name = Newname;
-            cout<<"New name is"<<name<<endl;
+            cout << "New name is" << name << endl;
         }
         else
-            cout<<"Sorry no words more than 10"<<endl;
-
-
-
-
+            cout << "Sorry no words more than 10" << endl;
     }
-
-
-
 };
 
+int main()
+{
 
+    employee em("Hasib", 22, 3000);
 
+    cout << em.getAge() << endl;
 
-int main(){
-
-employee em("Hasib",22,3000);
-
-cout<<em.getAge()<<endl;
-
-em.setName("Arshad");
-
-
+    em.setName("Arshad");
 }
