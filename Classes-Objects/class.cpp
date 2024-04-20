bcd9ad0c95e7dfa1212314d845b321a996c1ed47
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-20 18:49:23 +0200

    Created Class name Animal and added access specifier,attributes.

---
+++ Classes-Objects/class.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class Animal{  // The Class
+    public:         //Access Specifier
+        int Num_of_Animal; //Attribute {int variable}  
+        string myString;    //Attribute {String variable}
+
+
+};
