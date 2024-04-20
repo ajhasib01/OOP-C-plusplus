0e395beea5a48d03da30233a1f3f2bb530e28660
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-20 20:49:16 +0200

    Modified the code and added notes

---
+++ Access-Specifiers/Protected-specifier.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Animal{  // Base Class
+    protected:         //Access Specifier (Protected - No one can Access)
+        int Num_of_Animal; //Attribute {int variable}  
+       
+
+
+};
+
+//Create a Sub class name mammals
+class mammals:public Animal{
+
+    public:
+        void setNum(int Num){
+            // Child or Sub class is able to access the inherited 
+        
+           Num_of_Animal = Num;
+
+           // protected data members of base class
+        }
+ void displayNum() 
+    { 
+        cout << "Num_OF_Animal from from protected Access Specifier is: " << Num_of_Animal << endl; 
+    } 
+};
+
+int main() 
+{ 
+  
+    mammals obj; //Create an Object from mammal's class
+  
+    // member function of the sub class can 
+    // access the protected data members of the base class 
+  
+    obj.setNum(11); 
+    obj.displayNum(); 
+    return 0; 
+} 
