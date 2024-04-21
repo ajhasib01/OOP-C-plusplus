8b7a5f68cfedfe270489a0fcc563e9117bb679e2
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-21 23:15:27 +0200

    Created constructor.

---
+++ Constructor/constructor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Animal{  // The Class
+    public:         //Access Specifier
+       
+       Animal(){
+        cout<<"TIGER"<<endl;
+       }
+};
+
+int main(){
+
+    Animal myObj;
+    return 0;
+
+}
