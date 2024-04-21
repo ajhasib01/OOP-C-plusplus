c9b4103c38f32eb1fbe8ae4bd992f6c41732f811
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-21 22:27:01 +0200

    Added Inside method.

---
+++ Class-Methods/inside-method.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Animal{  // The Class
+    public:         //Access Specifier
+        void Inside_Method(int num) //Method or function defined inside the class
+        {
+            cout<<num<<"Animal"<<endl;
+        }
+
+
+};
+
+int main(){
+    Animal myObj1;// create an object of Animal base class.
+    myObj1.Inside_Method(10); // Calling Inside method.
+
+}
