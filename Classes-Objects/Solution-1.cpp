940817a720567a97c874c49c504c076b54e44717
ajhasib01 <hasibarshadjarif@gmail.com>
2024-04-22 02:10:22 +0200

    Created Outside constructor file.

--- Classes-Objects/Solution-1.cpp
+++ Classes-Objects/Solution-1.cpp
@@ -6,15 +6,7 @@ class Point
     double coord_x, coord_y;
 
 public:
-
-    Point(double coord_x, double coord_y) : coord_x(coord_x), coord_y(coord_y) {}
-    double get_coord_x()const{return coord_x}
-    double get_coord_y()const{return coord_y}
-    void set_coord_x(double x){ x = coord_x; return x}
-    void set_coord_y(double y){ y = coord_y; return y}
-	    			
-
-
+    double coord(double coord_x, double coord_y) : coord_x(coord_x), coord_y(coord_y) {}
 };
 
 int main()
