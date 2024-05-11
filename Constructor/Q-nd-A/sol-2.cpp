4e0b08aa5df4106b8dcf58f0dc180754759d586d
Hasib vai <hasibarshadjarif@gmail.com>
2024-05-11 21:20:06 +0200

    Created Base class named request.

---
+++ Constructor/Q-nd-A/sol-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+//Base class
+
+class Request{ //In the question it's says a class HttpRequest which extends Request.
+                //Here extends mean Request is the base class and HttpRequest will be the sub-class.
+    
+
+};
