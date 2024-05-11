4ae1848bfdc5559b7104f2f5ee72a71b7422d9b8
Hasib vai <hasibarshadjarif@gmail.com>
2024-05-11 21:23:23 +0200

    Added Virtual operator to log so that we can moodify it in infuture.

--- Constructor/Q-nd-A/sol-2.cpp
+++ Constructor/Q-nd-A/sol-2.cpp
@@ -5,6 +5,10 @@
 
 class Request{ //In the question it's says a class HttpRequest which extends Request.
                 //Here extends mean Request is the base class and HttpRequest will be the sub-class.
-    
+public:
+    virtual void log(){
+        std::cout<<"Request for loging"<<std::endl;
+    }
 
 };
+
