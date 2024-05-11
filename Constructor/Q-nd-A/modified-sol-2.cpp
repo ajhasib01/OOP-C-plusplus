4bb7a0bf3ee34d752b0fa8c2b90a11d93c91eb85
Hasib vai <hasibarshadjarif@gmail.com>
2024-05-11 22:02:41 +0200

    Created a sub-class which inherits from Request and added setter, getter and override the log function.

--- Constructor/Q-nd-A/modified-sol-2.cpp
+++ Constructor/Q-nd-A/modified-sol-2.cpp
@@ -19,3 +19,23 @@ public:
     }
 
 };
+
+class HttpsRequest:public Request{
+    std::string url;
+public:
+    HttpsRequest(std::string _url,std::string _user):Request(_user),url(_url){}
+
+    std::string get_url(){
+        return url;
+    }
+
+    void set_url(std::string _url){
+
+        url = _url;
+    }
+
+    void log()override{
+        std::cout<<"Https Request of from "<<user << " to "<< url <<std::endl;
+    }
+
+};
