30384ace2b1218afae2a9c2491e6f2e9278d61c6
Hasib vai <hasibarshadjarif@gmail.com>
2024-05-18 14:51:42 +0200

    Added Sub-class.
    Inside sub-class added constructor and override the log function.

--- Inheritance/Q-nd-A/sol-4.cpp
+++ Inheritance/Q-nd-A/sol-4.cpp
@@ -23,3 +23,24 @@ public:
     virtual void log() {} // Virtual method to be overridden by derived classes, provides default implementation
 };
 
+
+class GetRequest: public HttpRequest // Define a sub-class named GetRequest which inherits from HttpRequest
+{
+    std::string url; // Define a private member variable url to hold the URL
+
+public:
+    GetRequest(unsigned char a, unsigned char b, unsigned char c, unsigned char d, std::string url)
+        : HttpRequest(a, b, c, d), url(url) // Constructor for GetRequest, initializes IP address components and url
+    {}
+
+    void log() override // Override the log method from the base class
+    {
+        // Print an HTTP GET request message including the IP address components and the URL
+        std::cout << "Http GET Request from "
+                  << static_cast<int>(getData(0)) << "."
+                  << static_cast<int>(getData(1)) << "."
+                  << static_cast<int>(getData(2)) << "."
+                  << static_cast<int>(getData(3))
+                  << " to " << url << std::endl;
+    }
+};
