c8d930b7b112e675b2948bd5f0399fab373045b0
Hasib vai <hasibarshadjarif@gmail.com>
2024-05-18 14:38:11 +0200

    Added base class and created a virtual function.

---
+++ Inheritance/Q-nd-A/sol-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <string>   
+
+class HttpRequest // Define a Base class named HttpRequest
+{
+
+public:
+
+
+    virtual void log() // Define a virtual function log which will be overridden by derived classes
+    {
+
+    }
+
+};
