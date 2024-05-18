bc93135ee5d3030e6f4f2b93b81db3592734eee7
Hasib vai <hasibarshadjarif@gmail.com>
2024-05-18 14:39:42 +0200

    Added a sub class.
    This sub-class inherits from Base class. Also created a constructor and
    override log function.

--- Inheritance/Q-nd-A/sol-3.cpp
+++ Inheritance/Q-nd-A/sol-3.cpp
@@ -13,3 +13,20 @@ public:
     }
 
 };
+
+class GetRequest:public HttpRequest // Define a class named GetRequest which inherits from HttpRequest
+{
+    std::string url; // Define a private member variable named url of type string
+
+public:
+    GetRequest(std::string _url) // Constructor for GetRequest taking a string argument
+        : url(_url) // Initialize the url member variable with the provided value
+    {}
+
+    void log() override // Override the log method from the base class
+    {
+        std::cout << "Http GET Request of from a.b.c.d to " << url << std::endl; // Print a message indicating an HTTP GET request
+    }
+
+
+};
